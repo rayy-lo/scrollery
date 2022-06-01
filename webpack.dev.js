a64d6d03436f1dd3f5295b2477e1ec57dc9ad32d
Raymond Lo <raymond.lo@yahoo.com>
2022-06-01 15:15:31 -0400

    Add webpack

---
+++ webpack.dev.js
@@ -0,0 +1,13 @@
+/* eslint-env node */
+/* eslint @typescript-eslint/no-var-requires: "off" */
+
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist'
+  }
+});
---
+++ webpack.prod.js
@@ -0,0 +1,14 @@
+/* eslint-env node */
+/* eslint @typescript-eslint/no-var-requires: "off" */
+
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'production',
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  }
+});
