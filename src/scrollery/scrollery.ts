69b990fc5a103617f2d8b81b50d679c090e2b810
Raymond Lo <raymond.lo@yahoo.com>
2022-06-24 18:00:46 -0400

    Move init logic to builder

--- src/scrollery/scrollery.ts
+++ src/scrollery/scrollery.ts
@@ -1,34 +1,14 @@
-import Observer from '../observer/observer';
-import { Config, InitConfig } from '../types/config';
-import document from '../var/document';
-
 class Scrollery {
-  private loading: boolean;
-  private observer: IntersectionObserver;
-  private container: Element | null;
+  private path: string;
+  private static hasNextPage = true;
 
-  constructor(container: string, config: InitConfig);
-  constructor(container: Element, config: InitConfig);
-  constructor(container: string | Element, config: InitConfig) {
-    this.loading = false;
+  constructor(path: string) {
+    this.path = path;
+  }
 
-    if (typeof container === 'string') {
-      this.container = document.querySelector(container);
-    } else {
-      this.container = container;
-    }
-
-    const { threshold, root, rootMargin } = config;
-    const observerOptions: IntersectionObserverInit = {
-      threshold,
-      root,
-      rootMargin
-    };
-
-    this.observer = Observer.createObserver(
-      this.container?.lastElementChild as Element,
-      observerOptions
-    );
+  public static loadNextPage() {
+    if (!this.hasNextPage) return;
+    console.log('Load next page content');
   }
 }
 
