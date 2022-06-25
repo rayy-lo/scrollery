b0d49971f6421b39e4ae54a156b55563f73a7bf7
Raymond Lo <raymond.lo@yahoo.com>
2022-06-24 21:00:16 -0400

    Config types

--- src/types/config.ts
+++ src/types/config.ts
@@ -1,6 +1,24 @@
-export interface Config extends IntersectionObserverInit {
+export default interface ScrolleryConfig extends IntersectionObserverInit {
+  /**
+   * Query string to get next page
+   */
   path: string;
+  /**
+   * Selector to retrieve content to append to container
+   * e.g. ".post"
+   */
+  content: string;
+  /**
+   * Whether to check for the last page
+   */
   checkLastPage: boolean;
+  /**
+   * Intersection observer option properties
+   */
+  threshold: number | number[];
+  rootMargin: string;
+  /**
+   * Display spinner when loading next page content
+   */
+  showSpinner: boolean;
 }
-
-export type InitConfig = Partial<Config> & Required<Pick<Config, 'path'>>;
