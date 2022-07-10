b3e77b0b1bdcaa145550e67db94afe6646de4c20
Raymond Lo <raymond.lo@yahoo.com>
2022-07-10 18:23:59 -0400

    Create method to fetch next page content

--- src/scrollery/scrollery.ts
+++ src/scrollery/scrollery.ts
@@ -4,21 +4,39 @@ import { IScrollery, EventMap, ScrolleryEvents } from '../types/scrollery';
 class Scrollery implements IScrollery {
   public readonly config: ScrolleryConfig;
   handlers: EventMap = {};
+  pagination_number = 2;
 
   constructor(config: ScrolleryConfig) {
     this.config = Object.freeze(config);
   }
 
-  public loadNextPage() {
-    //TODO: Implement
+  fetchNextPageContent() {
+    /**
+     * TODO: Ability to fetch to different origins
+     */
+    const fetch_url: string =
+      window.location.origin +
+      window.location.pathname +
+      '?' +
+      this.config.path +
+      '=' +
+      this.pagination_number;
 
+    return fetch(fetch_url, this.config.fetchOptions)
+      .then((response) => response.text())
+      .then((data) => {
+        this.pagination_number++;
+        return data;
+      });
+  }
+
+  public async loadNextPage() {
+    const nextContent = await this.fetchNextPageContent();
+    console.log(nextContent);
     this.trigger('load');
   }
 
-  public on?(
-    event: ScrolleryEvents,
-    eventHandler: () => void
-  ): void {
+  public on?(event: ScrolleryEvents, eventHandler: () => void): void {
     if (event === 'load') {
       this.handlers['load'] = eventHandler;
     }
