9a0cc3530ced228e35640196548ac2e005055a83
Raymond Lo <raymond.lo@yahoo.com>
2022-07-19 09:58:03 -0400

    add test environment config options

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,11 @@
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
+  preset: 'ts-jest',
   verbose: true,
-  testEnvironment: 'jsdom'
+  testEnvironment: './test/env/test-env.ts',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  }
 };
 export default config;
