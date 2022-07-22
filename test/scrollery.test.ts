5d402494c065c2c05a7d2df6fbc7e01dc46099c1
Raymond Lo <raymond.lo@yahoo.com>
2022-07-22 02:34:32 -0400

    event tests for .on and .off

--- test/scrollery.test.ts
+++ test/scrollery.test.ts
@@ -11,26 +11,44 @@ let container;
 beforeAll(() => {
   window = globalThis.window;
   container = window.document.querySelector(globalThis.containerSelector);
+});
+
+beforeEach(() => {
   scrollery = new Scrollery(container, globalThis.config);
 });
 
-describe('Scrollery', () => {
-  describe('Events', () => {
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    const eventCallback = () => {};
-    it('should attach a callback fn for the load event', () => {
-      scrollery.on?.('load', eventCallback);
+afterEach(() => {
+  scrollery = null;
+});
 
-      expect(scrollery.handlers).toHaveProperty('load', eventCallback);
-    });
+describe('Scrollery Event System', () => {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  const eventCallback = () => {};
+  it('should attach a callback fn for the load event', () => {
+    scrollery.on('load', eventCallback);
 
-    it('should throw an error for non-event strings', () => {
-      expect(() => {
-        scrollery
-          .on('fake event', eventCallback)
-          .toThrow(/not a possible Scrollery event/);
-      });
+    expect(scrollery.handlers).toHaveProperty('load', eventCallback);
+  });
+
+  it('should throw an error for non-event strings', () => {
+    expect(() => {
+      scrollery
+        .on('fake event', eventCallback)
+        .toThrow(/not a possible Scrollery event/);
     });
   });
+
+  it('should invoke the corresponding event handler', () => {
+    scrollery.handlers['load'] = jest.fn();
+    scrollery.trigger('load');
+    expect(scrollery.handlers['load']).toHaveBeenCalled();
+  });
+
+  it('should remove event handler for specific event', () => {
+    scrollery.on('load', eventCallback);
+    expect(scrollery.handlers).toHaveProperty('load', eventCallback);
+
+    scrollery.off('load');
+    expect(scrollery.handlers).not.toHaveProperty('load');
+  });
 });
-``;
