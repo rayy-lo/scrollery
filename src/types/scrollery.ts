de77355ed80d271deac48c98f627d0374d014620
Raymond Lo <raymond.lo@yahoo.com>
2022-07-24 15:25:55 -0400

    clean up types

--- src/types/scrollery.ts
+++ src/types/scrollery.ts
@@ -1,12 +1,6 @@
-import ScrolleryConfig from './config';
-
 export type ScrolleryEvents = 'load' | 'last' | 'insert';
 
-export interface EventHandlers {
-  load?: () => void;
-  last?: () => void;
-  insert?: () => void;
-}
+export type EventHandlers = Partial<Record<ScrolleryEvents, () => void>>;
 
 export interface EventSystem {
   on(event: ScrolleryEvents, eventHandler: () => void): void;
@@ -15,7 +9,5 @@ export interface EventSystem {
 }
 
 export interface IScrollery {
-  config: ScrolleryConfig;
-  handlers: EventHandlers;
   loadNextPage(): void;
 }
