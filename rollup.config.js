ca2cf8c15518ae83839206deb7fc5782d6e9f8c3
Raymond Lo <raymond.lo@yahoo.com>
2022-08-05 23:33:10 -0400

    fix rollup build config

--- rollup.config.js
+++ rollup.config.js
@@ -9,40 +9,42 @@ dotenv.config();
 // eslint-disable-next-line no-undef
 const isProduction = process.env.NODE_ENV === 'production';
 
-const development_config = [
-  {
-    input: 'src/index.ts',
-    output: {
-      name: 'Scrollery',
-      file: 'client/assets/scrollery.js',
-      format: 'iife',
-      sourcemap: true
-    },
-    plugins: [
-      typescript({
-        tsconfig: './tsconfig.json'
-      }),
-      InlineSvg(),
-      serve('client/assets')
-    ]
-  }
+const plugins = [
+  typescript({
+    tsconfig: './tsconfig.json'
+  }),
+  InlineSvg()
 ];
+
+const development_config = {
+  input: 'src/index.ts',
+  output: {
+    name: 'Scrollery',
+    file: 'client/assets/scrollery.js',
+    format: 'iife',
+    sourcemap: true
+  },
+  plugins: [...plugins, serve('client/assets')]
+};
 
 const production_config = [
   {
     input: 'src/index.ts',
-    output: {
-      name: 'Scrollery',
-      file: 'dist/scrollery.min.js',
-      format: 'iife',
-      sourcemap: false
-    },
-    plugins: [
-      typescript({
-        tsconfig: './tsconfig.json'
-      }),
-      InlineSvg(),
-      terser()
+    plugins: [...plugins],
+    output: [
+      {
+        file: 'dist/scrollery.min.js',
+        format: 'iife',
+        sourcemap: false,
+        name: 'Scrollery',
+        plugins: [terser()]
+      },
+      {
+        file: 'dist/scrollery.js',
+        format: 'iife',
+        name: 'Scrollery',
+        sourcemap: false
+      }
     ]
   }
 ];
